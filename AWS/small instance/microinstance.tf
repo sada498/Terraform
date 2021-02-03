866409c678c5e9aaea543ccae6c33ea40f1ff231
sada498 <sada.y@outlook.com>
2021-02-03 15:21:43 +1100

    aws small instance run

---
+++ AWS/small instance/microinstance.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-047a51fa27710816e"
+  instance_type = "t2.micro"
+}
+resource "aws_default_vpc" "default" {
+
+}
