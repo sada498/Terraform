caef04887f9d693a742eff3647db3d1144867972
sada498 <sada.y@outlook.com>
2021-02-03 18:06:29 +1100

    v 0.1

--- AWS/small instance/microinstance.tf
+++ AWS/small instance/microinstance.tf
@@ -9,11 +9,17 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  #region  = "us-east-1"
+  #you can use variables.tf filw to configure values
+  region = var.region
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-047a51fa27710816e"
+    #Amazon Linux
+    #ami  = "ami-047a51fa27710816e"
+
+  # Red Hat Enterprise Linux
+   ami = "ami-096fda3c22c1c990a"
   instance_type = "t2.micro"
 }
 resource "aws_default_vpc" "default" {
