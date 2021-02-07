00474542bec9fd4b181c3347c85398a832cc90a4
sada498 <sada.y@outlook.com>
2021-02-07 21:19:37 +1100

    aws_vpc-2_instance-dns-s3

--- GCP/gcp_instance/main.tf
+++ GCP/gcp_instance/main.tf
@@ -19,6 +19,9 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-test-network1"
 }
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
@@ -36,6 +39,7 @@ resource "google_compute_instance" "vm_instance" {
   network_interface {
     network = google_compute_network.vpc_network.name
     access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
     }
   }
 }
